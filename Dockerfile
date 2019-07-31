016bc2dc32f1cd14c17c4a97f224f8e8373a8386
Matthes Rieke <m.rieke@52north.org>
2019-07-31 15:27:13 +0200

    initial setup

---
+++ Dockerfile
@@ -0,0 +1,76 @@
+# -- CLONE the latest javaps and iohandler
+FROM alpine/git as CLONE
+WORKDIR /usr/src
+RUN echo "invalidate the cache with another echo string"
+RUN git clone https://github.com/52north/javaPS.git app \
+    && git clone https://github.com/52North/javaps-iohandler.git app/javaps-iohandler \
+	&& git -C ./app/javaps-iohandler checkout develop \
+	&& git -C ./app checkout develop
+	
+# -- BUILD all components
+FROM maven:3.5-jdk-8-alpine as BUILD 
+WORKDIR /usr/src/app
+COPY --from=CLONE /usr/src/app /usr/src/app
+COPY ./javaps/javaps-pom.xml /usr/src/app/pom.xml
+RUN mvn -f ./pom.xml clean install -DskipTests -pl !webapp
+RUN mvn -f ./javaps-iohandler/pom.xml clean install
+COPY ./javaps/webapp-pom.xml /usr/src/app/webapp/pom.xml
+RUN mvn -f ./pom.xml install -DskipTests
+
+# -- RUNNABLE image based on jetty
+FROM jetty:jre8
+
+ARG JAVAPS_VERSION=1.4.0-SNAPSHOT
+ENV JAVAPS_VERSION ${JAVAPS_VERSION}
+ENV JAVAPS_ROOT ${JETTY_BASE}/webapps/ROOT
+ENV JAVAPS_TMP ${JAVAPS_ROOT}/WEB-INF/tmp
+ENV JAVAPS_CONFIG ${JAVAPS_ROOT}/WEB-INF/config
+ENV JAVAPS_LIB ${JAVAPS_ROOT}/WEB-INF/lib
+
+COPY --from=BUILD /usr/src/app/webapp/target/javaPS-webapp-${JAVAPS_VERSION}/ /var/lib/jetty/webapps/ROOT
+COPY etc/docker-log4j2.xml /var/lib/jetty/webapps/ROOT/WEB-INF/config/log4j2.xml
+COPY etc/docker-configuration.json /var/lib/jetty/webapps/ROOT/WEB-INF/config/configuration.json
+
+USER root
+RUN set -ex \
+ && apt-get update \
+ && apt-get install -y --no-install-recommends jq \
+ && rm -rf /var/lib/apt/lists/* \
+ && wget -q -P /usr/local/bin https://raw.githubusercontent.com/52North/arctic-sea/master/etc/faroe-entrypoint.sh \
+ && chmod +x /usr/local/bin/faroe-entrypoint.sh \
+ && ln -sf ${JAVAPS_CONFIG}/log4j2.xml ${JAVAPS_ROOT}/WEB-INF/classes/log4j2.xml \
+ && mkdir -p ${JAVAPS_TMP}\
+ && chown -R jetty:jetty ${JAVAPS_ROOT}
+
+COPY ./faroe-entrypoint.sh /usr/local/bin/faroe-entrypoint.sh
+RUN chmod +x /usr/local/bin/faroe-entrypoint.sh
+
+USER jetty
+
+VOLUME /var/lib/jetty/webapps/ROOT/WEB-INF/tmp
+VOLUME /var/lib/jetty/webapps/ROOT/WEB-INF/config
+
+HEALTHCHECK --interval=5s --timeout=20s --retries=3 \
+  CMD wget 'http://localhost:8080/service?service=WPS&request=GetCapabilities' -q -O - > /dev/null 2>&1
+
+ENV FAROE_CONFIGURATION ${JAVAPS_CONFIG}/configuration.json
+
+LABEL maintainer="Benjamin Proß <b.pross@52north.org>" \
+      org.opencontainers.image.title="52°North javaPS" \
+      org.opencontainers.image.description="Next generation standardized web-based geo-processing" \
+      org.opencontainers.image.licenses="Apache-2.0" \
+      org.opencontainers.image.url="https://github.com/52North/javaPS" \
+      org.opencontainers.image.vendor="52°North GmbH" \
+      org.opencontainers.image.source="https://github.com/52north/javaPS.git" \
+      org.opencontainers.image.documentation="https://github.com/52North/javaPS/blob/develop/README.md" \
+      org.opencontainers.image.version="${JAVAPS_VERSION}" \
+      org.opencontainers.image.authors="Benjamin Proß <b.pross@52north.org>, Christian Autermann <c.autermann@52north.org>"
+
+ARG GIT_COMMIT
+LABEL org.opencontainers.image.revision "${GIT_COMMIT}"
+
+ARG BUILD_DATE
+LABEL org.opencontainers.image.created "${BUILD_DATE}"
+
+CMD [ "java", "-jar", "/usr/local/jetty/start.jar" ]
+ENTRYPOINT [ "/usr/local/bin/faroe-entrypoint.sh", "/docker-entrypoint.sh" ]
---
+++ faroe-entrypoint.sh
@@ -0,0 +1,165 @@
+#!/bin/sh
+
+#
+# Script to transfer configuration parameters from 
+# environment variables to a Faroe configuration 
+# using either the SQLITE or the JSON backend.
+#
+# Settings are applied to the configuration in 
+# ${FAROE_CONFIGURATION}.
+#
+# Environment variables are expected to be all uppercase
+# with only underscores between the words. The mapping
+# depends on the exising keys in the configuration and
+# the environment variables will be matched against these,
+# e.g. SERVICE_IDENTIFICATION_TITLE will be applied to 
+# the setting serviceIdentification.title.
+#
+# Multilingual settings are expected to be in their JSON 
+# encoding, e.g.: '{"en":"english","de":"deutsch"}'.
+#
+# All command line arguments will be forwarded to exec.
+#
+# Requirements:
+# - jq
+# - sqlite3 (only for SQLITE configurations)
+# - tr
+# - sed
+#
+
+
+set -e
+
+log() { echo $* >&2; }
+
+identifier_to_env() {
+   sed -E -e 's|([a-z])([A-Z])|\1_\2|g; s|[-.]|_|g' | tr '[:lower:]' '[:upper:]'
+}
+
+process_settings_sqlite() {
+  local ENV_VARIABLE KEY VALUE TYPE
+  get_setting_identifiers_sqlite | while read KEY; do
+    ENV_VARIABLE="$(echo "${KEY}" | identifier_to_env)"
+    if printenv "${ENV_VARIABLE}" 2>&1 >/dev/null; then
+      VALUE="$(printenv "${ENV_VARIABLE}")"
+      TYPE="$(get_setting_type_sqlite "${KEY}")"
+      log "Applying ${ENV_VARIABLE} to setting ${KEY} of type ${TYPE}, value: ${VALUE}"
+      set_setting_sqlite "${KEY}" "${VALUE}" "${TYPE}"
+    fi
+  done
+}
+
+get_setting_identifiers_sqlite() {
+  sqlite3 "${FAROE_CONFIGURATION}" 'select identifier from settings;' | sort 
+}
+
+get_setting_type_sqlite() {
+  sqlite3 "${FAROE_CONFIGURATION}" "
+    SELECT 'boolean' As type FROM boolean_settings WHERE identifier = '$KEY'
+    UNION SELECT 'file' As type FROM file_settings WHERE identifier = '$KEY'
+    UNION SELECT 'integer' As type FROM integer_settings WHERE identifier = '$KEY'
+    UNION SELECT 'numeric' As type FROM numeric_settings WHERE identifier = '$KEY'
+    UNION SELECT 'string' As type FROM string_settings WHERE identifier = '$KEY'
+    UNION SELECT 'timeInstant' As type FROM time_settings WHERE identifier = '$KEY'
+    UNION SELECT 'uri' As type FROM uri_settings WHERE identifier = '$KEY'
+    UNION SELECT 'multilingual' As type FROM multilingual_string_settings WHERE identifier = '$KEY'"
+}
+
+set_setting_sqlite() {
+  local KEY="$1"
+  local VALUE="$2"
+  local TYPE="$3"
+  local SQL
+
+  if [ "${TYPE}" == "multilingual" ]; then
+    TABLE='multilingual_string_settings_values'
+    SQL="DELETE FROM ${TABLE} WHERE identifier = '${KEY}';"
+    
+    local IFS=$'\n'
+    for VALUES in $(echo "${VALUE}" | jq -r --jsonargs 'to_entries[] | "'"'"'\(.key)'"'"', '"'"'\(.value)'"'"'"'); do
+      SQL="${SQL} INSERT INTO ${TABLE} (identifier, lang, value) VALUES ('${KEY}', ${VALUES});"
+    done
+    sqlite3 "${FAROE_CONFIGURATION}" "${SQL}"
+  else
+    case "${TYPE}" in
+      string)       TABLE='string_settings' ;;
+      choice)       TABLE='choice_settings' ;;
+      file)         TABLE='file_settings' ;;
+      uri)          TABLE='uri_settings' ;;
+      timeInstant)  TABLE='time_settings' ;;
+      boolean)      TABLE='boolean_settings' ;;
+      integer)      TABLE='integer_settings' ;;
+      number)       TABLE='numeric_settings' ;;
+      *) log "WARN: unsupported settings type: ${TYPE}" ;;
+    esac
+    if [ "${VALUE}" == "null" ]; then
+      VALUE='NULL'
+    else
+      case "${TYPE}" in string|choice|file|uri|time) VALUE="'${VALUE}'" ;; esac
+    fi    
+    sqlite3 "${FAROE_CONFIGURATION}" "UPDATE ${TABLE} SET value = ${VALUE} WHERE identifier = '${KEY}'"
+  fi 
+
+}
+
+process_settings_json() {
+  local ENV_VARIABLE KEY VALUE TYPE
+  get_setting_identifiers_json | while read KEY; do
+    ENV_VARIABLE="$(echo "${KEY}" | identifier_to_env)"
+    #log "Checking for settings identifier $KEY in ENV VAR $ENV_VARIABLE"
+    if printenv "${ENV_VARIABLE}" 2>&1 >/dev/null; then
+      VALUE="$(printenv "${ENV_VARIABLE}")"
+      TYPE="$(get_setting_type_json "${KEY}")"
+      log "Applying ${ENV_VARIABLE} to setting ${KEY} of type ${TYPE}, value: ${VALUE}"
+      set_setting_json "${KEY}" "${VALUE}" "${TYPE}"
+    fi
+  done
+}
+
+get_setting_identifiers_json() {
+  jq -r '.settings | keys[]' "${FAROE_CONFIGURATION}"
+}
+
+get_setting_type_json() {
+  local KEY="$1"
+  jq -r --arg key "${KEY}" '.settings[$key].type' "${FAROE_CONFIGURATION}"
+}
+
+set_setting_json() {
+  local KEY="$1"
+  local VALUE="$2"
+  local TYPE="$3"
+  case "${TYPE}" in
+    # string based settings, add quotes
+    string|choice|file|uri|timeInstant)
+      if [ "${VALUE}" != "null" ]; then VALUE="\"${VALUE}\""; fi ;;
+    # basic json values, do nothing
+    boolean|integer|number) ;;
+    # complex json values, do noting
+    multilingual) ;;
+      #if [ "${VALUE}" != "null" ]; then VALUE="{\"en\":\"${VALUE}\"}"; fi ;;
+    *) log "WARN: unsupported settings type: ${TYPE}" ;;
+  esac
+
+  local TEMP="$(mktemp)"
+  jq --arg key "${KEY}" --argjson value "${VALUE}" '.settings[$key].value = $value' "${FAROE_CONFIGURATION}" > "${TEMP}"
+  mv -f "${TEMP}" "${FAROE_CONFIGURATION}"
+}
+
+if [ -z "${FAROE_CONFIGURATION}" ]; then
+  log "FAROE_CONFIGURATION is not defined"
+  exit 1
+fi
+
+if [ ! -f "${FAROE_CONFIGURATION}" ]; then
+  log "${FAROE_CONFIGURATION} does not exist"
+  exit 1
+fi
+
+case "${FAROE_CONFIGURATION}" in 
+  *json) process_settings_json ;;
+  *db) process_settings_sqlite ;;
+  *) log "Unrecognized settings format"; exit 1 ;;
+esac
+
+exec "$@"
